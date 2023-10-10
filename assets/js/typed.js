a9dc9e163a24d5a5d040cdd99ad257985e1b6c9b
nico <varela.nicoo@gmail.com>
2023-10-10 20:05:44 -0300

    firts repo

---
+++ assets/js/typed.js
@@ -0,0 +1,22 @@
+const typed = new Typed('.typed', {
+  strings: [
+            '<strong class="person">Know me</strong>', 
+            '<strong class="person">Contact me</strong>', 
+            '<strong class="person">Lets work together!</strong>'
+        ],
+        
+        // stringsELement: '#cadenas-texto', // ID del elemento que contiene cadenas de texto a mostrar.
+        typeSpeed: 75, // Velocidad en mlisegundos para poner una letra, 
+        startDelay: 500, // Tiempo de retraso en inictar la animacion. Aplica tambien cuando termina y vuelve a inictar, 
+        backSpeed: 75, // Velocidad en milisegundos para borrrar una letra, 
+        smartBackspace: true, // Elimtnar solamente las palabras que sean nuevas en una cadena de texto 
+        shuffle: false, // Alterar el orden en el que escribe las palabras.
+        backDelay: 1500, // Tiempo de espera despues de que termina de escribir una palabra.
+        loop: true, // Repetir el array de strings
+        loopCount: false, // Cantidad de veces a repetir el array. false = infinite
+        showcursor: true, // Mostrar cursor palpitanto
+        cursorChar: '↓', //caracter para el cursor
+        contentType: 'html' // 'html' o 'null' para texto sin formato
+        
+
+});
